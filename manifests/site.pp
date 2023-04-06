408819de2b1e8738fa3bf8e1cda9909b43de5cc9
so6erholic <55851776+so6erholic@users.noreply.github.com>
2023-04-06 15:00:20 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,9 @@
 node default {
+  file {'/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner => 'root',
+   }
 }
 node 'puppet-server.localdomain.lan' {
   include role::master_server
